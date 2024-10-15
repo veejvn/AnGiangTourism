eaba53b942af904af9299fde015f50d36153cdea
VeeJ <vesy17032003@gmail.com>
2024-10-15 08:19:05 +0700

    Second Commit

--- src/main/java/javaweb/AnGiangTourism/controller/DataController.java
+++ src/main/java/javaweb/AnGiangTourism/controller/DataController.java
@@ -7,15 +7,11 @@ import javaweb.AnGiangTourism.service.DataService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 @CrossOrigin(origins = "http://127.0.0.1:5500/")
 @RestController
